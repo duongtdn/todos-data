be0969be6626a9bf5ff1b47d766f2f30872380cc
duongtdn <duongtdn@gmail.com>
2017-03-30 09:05:14 +0700

    fix bug

--- src/taskGroup/actions.js
+++ src/taskGroup/actions.js
@@ -54,6 +54,7 @@ export const taskGroup = {
 
         if (Object.keys(members).length > 0) {
           for (let userId in members) {
+            if (userId === uid) { continue }
             const member = members[userId];
             const message = messages.template(TEMPLATE.INVITE_GROUP).create({
               receivers : [userId],
@@ -173,6 +174,7 @@ export const taskGroup = {
         // send invite message for inviting member
         const members = {...group.members};
         for (let id in members) {
+          if (userId === uid) { continue }
           const member = members[id];
           if (member && member.status === 'invited') {
             const message = messages.template(TEMPLATE.INVITE_GROUP).create({
