ccce7f970fd4cf9593b2c745837175ae879473a4
duongtdn <duongtdn@gmail.com>
2017-05-08 21:20:38 +0700

    use deply realtime db

--- src/firebase-services.js
+++ src/firebase-services.js
@@ -4,12 +4,12 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCrT408W7FS9Sx4iRCoVxllJTG8e6QhPuI",
-  authDomain: "todotogether-9ef1d.firebaseapp.com",
-  databaseURL: "https://todotogether-9ef1d.firebaseio.com",
-  projectId: "todotogether-9ef1d",
-  storageBucket: "todotogether-9ef1d.appspot.com",
-  messagingSenderId: "890607334661"
+  apiKey: "AIzaSyD1Dl4B0b2AD1zosQI1Y9moI1rU9cOrKEE",
+  authDomain: "todo-together-d1d99.firebaseapp.com",
+  databaseURL: "https://todo-together-d1d99.firebaseio.com",
+  projectId: "todo-together-d1d99",
+  storageBucket: "todo-together-d1d99.appspot.com",
+  messagingSenderId: "392354673787"
 };
 
 export const fb = firebase.initializeApp(config);
